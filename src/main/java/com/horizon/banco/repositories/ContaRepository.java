0cd25e6421d516800bda23f243a9cf09e24f90ac
carlosmelo4455 <carlos.melo4455@gmail.com>
2023-09-16 14:44:56 -0300

    metodos padrão para repositories

--- src/main/java/com/horizon/banco/repositories/ContaRepository.java
+++ src/main/java/com/horizon/banco/repositories/ContaRepository.java
@@ -1,7 +1,18 @@
 package com.horizon.banco.repositories;
 
 import com.horizon.banco.entities.Conta;
+import com.horizon.banco.entities.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ContaRepository extends JpaRepository<Conta, Long> {
+    Optional<Conta> findByNumeroAndDigito(String numero, String digito);
+
+    Optional<Conta> findByPessoaAndTipoConta(Pessoa pessoa, String tipoConta);
+
+    List<Conta> findByPessoa(Pessoa pessoa);
+
+    List<Conta> findByTipoConta(String tipoConta);
 }
--- src/main/java/com/horizon/banco/repositories/TransferenciaRepository.java
+++ src/main/java/com/horizon/banco/repositories/TransferenciaRepository.java
@@ -1,7 +1,14 @@
 package com.horizon.banco.repositories;
 
+import com.horizon.banco.entities.Conta;
 import com.horizon.banco.entities.Transferencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
+    List<Transferencia> findByContaOrigemOrContaDestino(Conta contaOrigem, Conta contaDestino);
+
+    List<Transferencia> findByData(Date data);
 }
