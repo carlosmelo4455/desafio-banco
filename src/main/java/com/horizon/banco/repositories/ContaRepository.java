63a0929a377aa857e91a549a9b96eab802eb13d7
carlosmelo4455 <carlos.melo4455@gmail.com>
2023-09-19 16:35:24 -0300

    1.0

--- src/main/java/com/horizon/banco/repositories/ContaRepository.java
+++ src/main/java/com/horizon/banco/repositories/ContaRepository.java
@@ -2,6 +2,7 @@ package com.horizon.banco.repositories;
 
 import com.horizon.banco.entities.Conta;
 import com.horizon.banco.entities.Pessoa;
+import com.horizon.banco.entities.enums.TipoConta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,11 +11,9 @@ import java.util.Optional;
 public interface ContaRepository extends JpaRepository<Conta, Long> {
     Optional<Conta> findByNumeroAndDigito(String numero, String digito);
 
-    Optional<Conta> findByPessoaAndTipoConta(Pessoa pessoa, String tipoConta);
+    List<Conta> findByPessoaAndTipoConta(Pessoa pessoa, TipoConta tipoConta);
 
     List<Conta> findByPessoa(Pessoa pessoa);
-
-    List<Conta> findByTipoConta(String tipoConta);
 
     List<Conta> findByPessoaId(Long pessoaId);
 }
--- src/main/java/com/horizon/banco/repositories/PessoaRepository.java
+++ src/main/java/com/horizon/banco/repositories/PessoaRepository.java
@@ -2,13 +2,10 @@ package com.horizon.banco.repositories;
 
 import com.horizon.banco.entities.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
     Optional<Pessoa> findByCpf(String cpf);
-
     List<Pessoa> findByNome(String nome);
-
 }
